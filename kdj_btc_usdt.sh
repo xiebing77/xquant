9f2911bf1a84a374636d35762bfa7dd167a42495
xb <xiebing77@gmail.com>
2018-08-12 02:36:12 +0800

    binance的btc精度改为6

--- kdj_btc_usdt.sh
+++ kdj_btc_usdt.sh
@@ -3,4 +3,4 @@
 source ~/.profile
 base_dir=$(cd `dirname $0`; pwd)
 echo $1
-python3.6 $base_dir/main.py strategy/kdj KDJStrategy '{"symbol":"btc_usdt", "digits":{"btc":8,"usdt":2}, "exchange":"binance", "sec": 600, "limit":200, "id": "x10"}' $1
+python3.6 $base_dir/main.py strategy/kdj KDJStrategy '{"symbol":"btc_usdt", "digits":{"btc":6,"usdt":2}, "exchange":"binance", "sec": 600, "limit":200, "id": "x11"}' $1
--- mixedkdj_btc_usdt.sh
+++ mixedkdj_btc_usdt.sh
@@ -4,4 +4,4 @@
 source ~/.profile
 base_dir=$(cd `dirname $0`; pwd)
 echo $1
-python3.6 $base_dir/main.py strategy/mixedkdj MixedKDJStrategy '{"symbol":"btc_usdt", "digits":{"btc":8,"usdt":2}, "exchange":"binance", "sec": 600, "limit":200, "id": "x12"}' $1
+python3.6 $base_dir/main.py strategy/mixedkdj MixedKDJStrategy '{"symbol":"btc_usdt", "digits":{"btc":6,"usdt":2}, "exchange":"binance", "sec": 600, "limit":200, "id": "x12"}' $1
