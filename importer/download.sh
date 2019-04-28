b28fef5df4e232ad34f30a508d007362ad51f1e1
Bing Xie <xiebing77@gmail.com>
2019-04-28 16:57:46 +0800

    新增下载btc_usdc

--- importer/download.sh
+++ importer/download.sh
@@ -7,7 +7,7 @@ echo $range
 mds=(binance)
 echo ${mds[*]}
 
-symbles=(btc_usdt bnb_usdt eth_usdt btc_pax)
+symbles=(btc_usdt bnb_usdt eth_usdt btc_pax btc_usdc)
 echo ${symbles[*]}
 
 kline_types=(1m 5m 15m 30m 1h 2h 4h 6h 8h 12h 1d)
