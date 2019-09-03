15136f83048157c3e73b792d139d96e9415d18c4
Bing Xie <xiebing77@gmail.com>
2019-09-03 16:24:28 +0800

    新增小工具limit_buy

---
+++ tools/limit_buy.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+sys.path.append('../')
+import argparse
+from exchange.binanceExchange import BinanceExchange
+import common.xquant as xq
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='account')
+    parser.add_argument('-e', help='exchange')
+    parser.add_argument('-s', help='symbol (btc_usdt)')
+    parser.add_argument('-p', help='price')
+    parser.add_argument('-a', help='amount')
+    args = parser.parse_args()
+    # print(args)
+    if not (args.e):
+        parser.print_help()
+        exit(1)
+
+    if args.e == "binance":
+        exchange = BinanceExchange(debug=True)
+    else:
+        print("exchange error!")
+        exit(1)
+
+    exchange.send_order(xq.DIRECTION_LONG, xq.OPEN_POSITION, xq.ORDER_TYPE_LIMIT, args.s, args.p, args.a)
