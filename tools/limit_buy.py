124b44984a2359e9a1b5c71dae3d7f29d6138976
JaceXie <xiebing77@gmail.com>
2019-09-03 16:34:05 +0800

    新增小工具limit_buy

--- tools/limit_buy.py
+++ tools/limit_buy.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     parser.add_argument('-a', help='amount')
     args = parser.parse_args()
     # print(args)
-    if not (args.e):
+    if not (args.e and args.s and args.p and args.a):
         parser.print_help()
         exit(1)
 
@@ -23,4 +23,4 @@ if __name__ == "__main__":
         print("exchange error!")
         exit(1)
 
-    exchange.send_order(xq.DIRECTION_LONG, xq.OPEN_POSITION, xq.ORDER_TYPE_LIMIT, args.s, args.p, args.a)
+    exchange.send_order(xq.DIRECTION_LONG, xq.OPEN_POSITION, xq.ORDER_TYPE_LIMIT, args.s, float(args.p), float(args.a))
