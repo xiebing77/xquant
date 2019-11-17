217a69bba1ab66bab223ed80af18160fd3fbe6a8
Gabriel <pkguowu@yahoo.com>
2019-11-17 11:50:20 +0800

    fix shell bug

--- scripts/auto_repay.sh
+++ scripts/auto_repay.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+OS=`uname -s`
+if [ ${OS} == "Darwin" ];then
+    source ~/.bash_profile
+elif [ ${OS} == "Linux" ]; then
+    source ~/.profile
+fi
 
-source ~/.profile
 base_dir=$(cd `dirname $0`; pwd)
 echo $1
-python3.6 $base_dir/../tools/auto_repay.py -e binance >> $base_dir/auto_repay.log 2>&1
+PYTHONPATH=$base_dir python3 $base_dir/../tools/auto_repay.py -e binance >> $base_dir/auto_repay.log 2>&1
