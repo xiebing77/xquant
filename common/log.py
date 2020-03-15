6ee233060ed26e8070750154f1705fedf830ea63
Gabriel <pkguowu@gmail.com>
2020-03-15 17:48:07 +0800

    add log server feature

--- common/log.py
+++ common/log.py
@@ -3,9 +3,13 @@
 
 import os
 import logging
+import logging.handlers
 
+logger = logging.getLogger('QuantLogger')
+logger.setLevel(logging.DEBUG)
 
-def init(path, logfilename):
+def init(path, logname):
+    global logger
     #logpath = os.path.join(os.getcwd(), "log")
     logpath = os.path.join("log")
     if not os.path.exists(logpath):
@@ -15,22 +19,28 @@ def init(path, logfilename):
     if not os.path.exists(fullpath):
         os.mkdir(fullpath)
 
-    filename = os.path.join(fullpath, logfilename)
+    filename = os.path.join(fullpath, logname)
     logging.basicConfig(level=logging.NOTSET, filename=filename)
 
+    extra = {'tags':logname}
+    handler = logging.handlers.SysLogHandler(address = ('127.0.0.1', 1514))
+    f = logging.Formatter('%(tags)s: %(message)s')
+    handler.setFormatter(f)
+    logger.addHandler(handler)
+    logger = logging.LoggerAdapter(logger, extra)
 
 def info(info):
-    logging.info(info)
+    logger.info(info)
 
 def warning(info):
-    logging.warning(info)
+    logger.warning(info)
 
 def error(info):
-    logging.error(info)
+    logger.error(info)
 
 def critical(info):
-    logging.critical(info)
+    logger.critical(info)
 
 def debug(info):
-    logging.debug(info)
+    logger.debug(info)
 
