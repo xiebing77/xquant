167bb4d1a07aaf9e1e7f40a284d1caacb2caac36
Gabriel <pkguowu@gmail.com>
2020-03-17 12:43:03 +0800

    change log server ip

--- common/log.py
+++ common/log.py
@@ -7,15 +7,16 @@ import logging.handlers
 logger = logging.getLogger('QuantLogger')
 logger.setLevel(logging.DEBUG)
 
-def init(logname):
+def init(logname, server_ip=None, server_port=None):
     global logger
     logging.basicConfig(level=logging.NOTSET, filename=logname)
-    extra = {'tags':logname}
-    handler = logging.handlers.SysLogHandler(address = ('127.0.0.1', 1514))
-    f = logging.Formatter('%(tags)s: %(message)s')
-    handler.setFormatter(f)
-    logger.addHandler(handler)
-    logger = logging.LoggerAdapter(logger, extra)
+    if server_ip != None:
+        extra = {'tags':logname}
+        handler = logging.handlers.SysLogHandler(address = (server_ip, int(server_port)))
+        f = logging.Formatter('%(tags)s: %(message)s')
+        handler.setFormatter(f)
+        logger.addHandler(handler)
+        logger = logging.LoggerAdapter(logger, extra)
 
 def info(info):
     logger.info(info)
