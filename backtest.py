2233398ea74be64e7df3e3ada7e8de4ccc118141
Bing Xie <xiebing77@gmail.com>
2020-03-23 12:17:18 +0800

    backetest独立2

--- backtest.py
+++ backtest.py
@@ -6,6 +6,7 @@ import json
 import uuid
 import utils.tools as ts
 import common.xquant as xq
+import common.log as log
 from engine.backtest import BackTest
 
 if __name__ == "__main__":
@@ -14,6 +15,7 @@ if __name__ == "__main__":
     parser.add_argument('-sc', help='strategy config')
     parser.add_argument('-r', help='time range (2018-7-1T8' + xq.time_range_split + '2018-8-1T8)')
     parser.add_argument('--cs', help='chart show', action="store_true")
+    parser.add_argument('--log', help='log', action="store_true")
     args = parser.parse_args()
     # print(args)
     if not (args.m and args.sc and args.r):
@@ -29,8 +31,16 @@ if __name__ == "__main__":
 
     module_name = config["module_name"].replace("/", ".")
     class_name = config["class_name"]
-    start_time, end_time = ts.parse_date_range(args.r)
+
+    time_range = args.r
+    start_time, end_time = ts.parse_date_range(time_range)
     instance_id = str(uuid.uuid1())  # 每次回测都是一个独立的实例
+
+    if args.log:
+        logfilename = class_name + "_"+ config["symbol"] + "_" + time_range + "_" + instance_id + ".log"
+        log.init("backtest", logfilename)
+        log.info("strategy name: %s;  config: %s" % (class_name, config))
+
 
     engine = BackTest(instance_id, args.m, config)
     strategy = ts.createInstance(module_name, class_name, config, engine)
