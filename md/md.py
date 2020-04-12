498bccd66e12c4f3a383cfdde8f250dee3fdcf1a
Bing Xie <xiebing77@gmail.com>
2020-04-12 12:13:49 +0800

    微调

--- md/md.py
+++ md/md.py
@@ -20,6 +20,19 @@ class MarketingData:
 
     def __init__(self, exchange):
         self.kline_column_names = get_kline_column_names(exchange)
+        for index, value in enumerate(self.kline_column_names):
+            if value == "high":
+                self.highindex = index
+            if value == "low":
+                self.lowindex = index
+            if value == "open":
+                self.openindex = index
+            if value == "close":
+                self.closeindex = index
+            if value == "volume":
+                self.volumeindex = index
+            if value == "open_time":
+                self.opentimeindex = index
 
     def get_kline_column_names(self):
         return self.kline_column_names
