2caae012ed810450aff7b740f15141938e104390
Bing Xie <xiebing77@gmail.com>
2020-04-12 16:50:16 +0800

    微调

--- md/md.py
+++ md/md.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python
-"""运行环境引擎"""
-import matplotlib.pyplot as plt
-import matplotlib.dates as dts
-from matplotlib import gridspec
-import mpl_finance as mpf
-import pandas as pd
-import talib
-from datetime import datetime,timedelta
-import common.log as log
-import utils.tools as ts
-import utils.indicator as ic
-import common.xquant as xq
-import db.mongodb as md
+"""marketing data"""
 from exchange.exchange import get_kline_column_names
 
 
