f6ad19f4c4e3eb0e924e77af5c81069d3c8ff739
Bing Xie <xiebing77@gmail.com>
2020-08-01 20:23:24 +0800

    add sum by balance tool

--- tools/balance.py
+++ tools/balance.py
@@ -2,25 +2,52 @@
 import sys
 sys.path.append('../')
 import argparse
-from exchange.exchange import create_exchange
+from exchange.exchange import create_exchange, exchange_names, get_kline_column_names
 from tabulate import tabulate as tb
 import pprint
 
+from common.xquant import creat_symbol, get_balance_free, get_balance_frozen
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='account')
-    parser.add_argument('-e', help='exchange, eg: binance, binance_margin')
+    parser = argparse.ArgumentParser(description='account infomation')
+    parser.add_argument('-exchange', choices=exchange_names, help='exchange name')
+    parser.add_argument('-basecoin', default='usdt', help='assert sum by base coin')
     args = parser.parse_args()
     # print(args)
-    if not (args.e):
+    if not (args.exchange):
         parser.print_help()
         exit(1)
 
-    exchange = create_exchange(args.e)
+    exchange = create_exchange(args.exchange)
     if not exchange:
-        print("exchange error!")
+        print("exchange name error!")
         exit(1)
 
     account = exchange.get_account()
+    balances = account['balances']
     print("account info:" )
     #pprint.pprint(account)
-    print(tb(account['balances']))
+    print(tb(balances))
+
+    kline_column_names = get_kline_column_names(exchange)
+    for index, value in enumerate(kline_column_names):
+        if value == "close":
+            closeindex = index
+            break
+
+    total_value = 0
+    for coin in balances:
+        amount = max(get_balance_free(coin), get_balance_frozen(coin))
+        if amount <= 0:
+            continue
+
+        symbol = creat_symbol(coin, args.basecoin)
+        klines = exchange.get_klines_1min(symbol, size=1)
+        price = klines[-1][closeindex]
+        value = price * amount
+        total_value += value
+        coin['value'] = value
+
+    print(tb(balances))
+    print("total value: %s  %s" % (total_value, args.basecoin))
