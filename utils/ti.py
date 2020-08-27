7de01fce9ac8dc48c538e16b056846b9ca6ecec6
Bing Xie <xiebing77@gmail.com>
2020-08-27 19:48:10 +0800

    fix bug for ti

--- utils/ti.py
+++ utils/ti.py
@@ -16,12 +16,12 @@ def EMA(kls, vkey, period):
 
     if key in kls[-2]:
         kls[-1][key] = float(kls[-1][vkey]) * k + kls[-2][key] * (1 - k)
-        return
+        return key
 
     if key in kls[-3]:
         kls[-2][key] = float(kls[-2][vkey]) * k + kls[-3][key] * (1 - k)
         kls[-1][key] = float(kls[-1][vkey]) * k + kls[-2][key] * (1 - k)
-        return
+        return key
 
     if len(kls) < period:
         return
@@ -33,7 +33,7 @@ def EMA(kls, vkey, period):
 
     for i in range(period, len(kls)):
         kls[i][key] = float(kls[i][vkey]) * k + kls[i-1][key] * (1 - k)
-
+    return key
 
 def get_bias_key(period_s, period_l):
     return "bias_%s_%s" % (period_s, period_l)
@@ -46,14 +46,14 @@ def BIAS(kls, period_s, period_l):
     kl = kls[-1]
     if key in kls[-2] and eskey in kl and elkey in kl:
         kl[key] = (kl[eskey] - kl[elkey]) / kl[elkey]
-        return
+        return key
 
     kl = kls[-2]
     if key in kls[-3] and eskey in kl and elkey in kl:
         kl[key] = (kl[eskey] - kl[elkey]) / kl[elkey]
         kl = kls[-1]
         kl[key] = (kl[eskey] - kl[elkey]) / kl[elkey]
-        return
+        return key
 
     if len(kls) < period_l:
         return
@@ -63,6 +63,7 @@ def BIAS(kls, period_s, period_l):
     for kl in kls:
         if eskey in kl and elkey in kl:
             kl[key] = (kl[eskey] - kl[elkey]) / kl[elkey]
+    return key
 
 
 def get_rsi_key(period):
@@ -84,7 +85,7 @@ def RSI(kls, vkey, period=14):
         kls[-1][ukey] = ema_u = py_rsi_ema(kls[-2][ukey], u, period)
         kls[-1][dkey] = ema_d = py_rsi_ema(kls[-2][dkey], d, period)
         kls[-1][key] = 100 * (ema_u / (ema_u + ema_d))
-        return
+        return key
 
     if key in kls[-3]:
         u, d = py_rsi_ua(float(kls[-3][vkey]), float(kls[-2][vkey]))
@@ -96,7 +97,7 @@ def RSI(kls, vkey, period=14):
         kls[-1][ukey] = ema_u = py_rsi_ema(kls[-2][ukey], u, period)
         kls[-1][dkey] = ema_d = py_rsi_ema(kls[-2][dkey], d, period)
         kls[-1][key] = 100 * (ema_u / (ema_u + ema_d))
-        return
+        return key
 
     if len(kls) < period:
         return
@@ -116,5 +117,6 @@ def RSI(kls, vkey, period=14):
         kls[i][ukey] = ema_u = py_rsi_ema(kls[i-1][ukey], u, period)
         kls[i][dkey] = ema_d = py_rsi_ema(kls[i-1][dkey], d, period)
         kls[i][key] = 100 * (ema_u / (ema_u + ema_d))
+    return key
 
 
